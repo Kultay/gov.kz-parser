ff1d3d2b2b395a4564aa80d2425ae4f6fd8b09df
Danik <d.mukhametkaliyev@qbs.kz>
2024-01-26 17:05:19 +0600

    Initial Commit

--- Organizations.py
+++ Organizations.py
@@ -4,7 +4,7 @@ import requests
 
 url = "https://www.gov.kz/graphql"
 
-languages = ['en', 'ru', 'kk']
+languages = ['en', 'kk', 'ru']
 
 items_dict = {}
 
@@ -14,10 +14,10 @@ for l in languages:
     }
     body = """
     {
-      projectdetails (_size:>1000){
+      projectdetails (_size:>0){
         id
         project_name 
-        supervisor{
+        supervisor {
           lastname_initials
           name
           lastname
@@ -34,30 +34,40 @@ for l in languages:
     organizations = data['data']['projectdetails']
 
     for i in organizations:
-        if i['supervisor'] is not None:
-            project_id = i['id']
-            if project_id not in items_dict:
-                items_dict[project_id] = [
-                    project_id,
-                    i['project_name'] if l == 'en' else None,
-                    i['project_name'] if l == 'kk' else None,
-                    i['project_name'] if l == 'ru' else None,
-                    i['supervisor']['lastname_initials'],
-                    i['supervisor']['name'],
-                    i['supervisor']['lastname'],
-                    i['supervisor']['middlename'],
-                    i['supervisor']['position'],
-                    i['supervisor']['phone'],
-                    i['supervisor']['email']
-                ]
-            else:
-                items_dict[project_id][1 + languages.index(l)] = i['project_name']
+        project_id = i['id']
+        if i['supervisor'] is None:
+            i['supervisor'] = {"lastname_initials": "0", "name": "0", "lastname": "0", "middlename": "0",
+                               "position": "0", "phone": "0", "email": "0"}
+
+        if project_id not in items_dict:
+            items_dict[project_id] = [
+                project_id,
+                i['project_name'] if l == 'en' else None,
+                i['project_name'] if l == 'kk' else None,
+                i['project_name'] ,
+                i['supervisor']['lastname_initials'],
+                i['supervisor']['name'],
+                i['supervisor']['lastname'],
+                i['supervisor']['middlename'],
+                i['supervisor']['position'],
+                i['supervisor']['phone'],
+                i['supervisor']['email']
+            ]
+        else:
+            items_dict[project_id][1 + languages.index(l)] = i['project_name']
+            items_dict[project_id][2 + languages.index(l)] = i['supervisor']['lastname_initials']
+            items_dict[project_id][3 + languages.index(l)] = i['supervisor']['name']
+            items_dict[project_id][4 + languages.index(l)] = i['supervisor']['lastname']
+            items_dict[project_id][5 + languages.index(l)] = i['supervisor']['middlename']
+            items_dict[project_id][6 + languages.index(l)] = i['supervisor']['position']
+            items_dict[project_id][7 + languages.index(l)] = i['supervisor']['phone']
+            items_dict[project_id][8 + languages.index(l)] = i['supervisor']['email']
+
 
 items_arr = list(items_dict.values())
 print(items_arr)
-header = ['id', 'Project_name_en', 'Project_name_ru', 'Project_name_kk', 'Lastname_initials', 'Name', 'Lastname',
+header = ['id', 'Project_name_en', 'Project_name_kk', 'Project_name_ru', 'Lastname_initials', 'Name', 'Lastname',
           'Middlename', 'Position', 'Phone', 'Email']
-
 
 with open("Organizations.csv", "w", newline='', encoding='utf-8') as f:
     w = csv.writer(f, delimiter=",")
